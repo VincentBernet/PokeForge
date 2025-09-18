7ab6db2e6e213f4d2eba179ac06e043e08a910f6
VincentBernet <vincent.bernet@lydia-app.com>
2025-09-18 23:33:26 +0200

    feat: init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	/**
+	 * TODO:
+	 * - Use a json file to store Pokemon names & ids.
+	 * - When a Pokemon is selected, contact openAI to generate a team based on the selected Pokemon.
+	 * - Store the team in a state
+	 * - Stream the response from openAI to display the team description as it's being generated.
+	 * - Use the PokeAPI to get the sprites of the Pokemon in the team.
+	 * - Display the team in a nice way.
+	 */
+	return (
+		<div className="flex min-h-screen flex-col items-center justify-center gap-6 bg-background p-4 pb-60 text-foreground">
+			<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance">
+				PokeForge
+			</h1>
+			<div className="card">
+				<Select>
+					<SelectTrigger className="w-[180px]">
+						<SelectValue placeholder="Select a fruit" />
+					</SelectTrigger>
+					<SelectContent>
+						<SelectGroup>
+							<SelectLabel>First Generation</SelectLabel>
+							<SelectItem value="pikachu">Pikachu</SelectItem>
+							<SelectItem value="bulbasaur">Bulbasaur</SelectItem>
+							<SelectItem value="charmander">Charmander</SelectItem>
+							<SelectItem value="squirtle">Squirtle</SelectItem>
+							<SelectItem value="pidgey">Pidgey</SelectItem>
+						</SelectGroup>
+					</SelectContent>
+				</Select>
+			</div>
+		</div>
+	);
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
